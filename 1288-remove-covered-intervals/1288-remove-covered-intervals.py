7d3e2f595867e9c55fb198412ba04e6aa4f11138
324fahim <147944832+324fahim@users.noreply.github.com>
2026-07-06 22:54:56 +0500

    Time: 7 ms (57.58%), Space: 12.6 MB (90.91%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def removeCoveredIntervals(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: int
+        """
+        intervals.sort(key=lambda x: (x[0], -x[1]))
+
+        ans = 0
+        max_end = -1
+
+        for start, end in intervals:
+            if end > max_end:
+                ans += 1
+                max_end = end
+
+        return ans
